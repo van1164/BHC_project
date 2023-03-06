17619132101127a821d92cd40d1e8d7f94279bb3
happyhyep <jhi2359@naver.com>
2023-03-06 11:59:57 +0900

    add map marker

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -3,14 +3,59 @@ import React, { useEffect } from "react";
 const {kakao} = window;
 
 function MapContainer() {
+    const positions = [
+        {
+            title: "용인 서천",
+            lat: 37.240951,
+            lng: 127.073126
+        },
+        {
+            title: "수원 영통",
+            lat: 37.248838,
+            lng: 127.076500
+        }
+    ];
+
+    const imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png"; 
+    
     useEffect(() => {
         const container = document.getElementById('map');
         const options = {
-            center: new kakao.maps.LatLng(33.450701, 126.570667),
-            level:3
+            center: new kakao.maps.LatLng(37.240951, 127.073126),
+            level:30
         };
         const map = new kakao.maps.Map(container, options);
-    }, [])
+        positions.map((el) => {
+            let marker = new kakao.maps.Marker({
+                map: map,
+                position: new kakao.maps.LatLng(el.lat, el.lng),
+            });
+            var infowindow = new kakao.maps.InfoWindow({
+                content: el.title, // 인포윈도우에 표시할 내용
+              });
+            kakao.maps.event.addListener(
+                marker,
+                "mouseover",
+                makeOverListener(map, marker, infowindow)
+            );
+                kakao.maps.event.addListener(
+                marker,
+                "mouseout",
+                makeOutListener(infowindow)
+            );
+            marker.setMap(map);
+        }, [])
+        })
+     function makeOverListener(map, marker, infowindow) {
+        return function () {
+            infowindow.open(map, marker);
+        };
+        }
+        function makeOutListener(infowindow) {
+            return function () {
+              infowindow.close();
+            };
+          }
 
     return (
         <div id="map" style={{
