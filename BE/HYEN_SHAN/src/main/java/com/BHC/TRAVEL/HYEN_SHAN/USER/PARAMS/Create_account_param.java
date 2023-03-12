2657c8ff4dcb5c993ec7a54dfbc741262890fcc7
Sihwan Kim <52437971+van1164@users.noreply.github.com>
2023-03-12 12:04:16 +0900

    update register BE

---
+++ BE/HYEN_SHAN/src/main/java/com/BHC/TRAVEL/HYEN_SHAN/USER/PARAMS/Create_account_param.java
@@ -0,0 +1,31 @@
+package com.BHC.TRAVEL.HYEN_SHAN.USER.PARAMS;
+
+public class Create_account_param {
+    String id;
+    String pw;
+    String Nick;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public String getNick() {
+        return Nick;
+    }
+
+    public void setNick(String nick) {
+        Nick = nick;
+    }
+}
--- BE/HYEN_SHAN/src/main/java/com/BHC/TRAVEL/HYEN_SHAN/USER/UserController.java
+++ BE/HYEN_SHAN/src/main/java/com/BHC/TRAVEL/HYEN_SHAN/USER/UserController.java
@@ -1,14 +1,13 @@
 package com.BHC.TRAVEL.HYEN_SHAN.USER;
 
 import com.BHC.TRAVEL.HYEN_SHAN.USER.MODEL.USER;
+import com.BHC.TRAVEL.HYEN_SHAN.USER.PARAMS.Create_account_param;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -19,17 +18,15 @@ import java.util.Map;
 @Controller
 @RequestMapping("/User")
 public class UserController {
-
     @Autowired
     MongoTemplate mongoTemplate;
     ObjectMapper objectMapper = new ObjectMapper();
     @ResponseBody
-    @GetMapping("test")
-    public String mapping_Test() throws JsonProcessingException {
-        USER user = USER.builder().ID("van123").password("ttink1245!").nickname("샨").build();
+    @PostMapping("Create_Account")
+    public String mapping_Test(@RequestBody Create_account_param param) throws JsonProcessingException {
+        USER user = USER.builder().ID(param.getId()).password(param.getPw()).nickname(param.getNick()).build();
         mongoTemplate.insert(user);
         String tempText = objectMapper.writeValueAsString(user);
-        System.out.println("TEST");
         return tempText;
     }
 
